97fa728a0c6ead3b5626c10b98825cd9689edc12
Tshepiso Mogapi <tshepisomogapi@Tshepisos-MacBook-Pro.local>
2019-10-17 04:26:21 +0200

    Initial commit for the project

---
+++ app/src/main/java/com/example/aad_team_51_animation_challenge/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.aad_team_51_animation_challenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
