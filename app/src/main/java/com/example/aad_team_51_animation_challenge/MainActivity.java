6480859f29ce128409c87b635cbc08337ce37381
Ewald Horn <ewaldhorn@gmail.com>
2019-10-17 12:27:56 +0200

    Migrating to AndroidX Updated to most recent, stable libraries and Android platform

--- app/src/main/java/com/example/aad_team_51_animation_challenge/MainActivity.java
+++ app/src/main/java/com/example/aad_team_51_animation_challenge/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.aad_team_51_animation_challenge;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
