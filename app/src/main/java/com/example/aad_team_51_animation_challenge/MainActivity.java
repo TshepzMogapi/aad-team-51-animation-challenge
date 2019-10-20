8b63509751ee0ab1f12265e1e73d6b8f9271eb4b
Ewald Horn <ewaldhorn@gmail.com>
2019-10-20 12:39:14 +0200

    Added basic navigation.
    Welcome Screen.
    Main Screen leading to a Game, High Scores, About and Question Management screen.
    All done with Navigation Component as fragments.  There's no layouts yet, this is just the navigation structure.

--- app/src/main/java/com/example/aad_team_51_animation_challenge/MainActivity.java
+++ app/src/main/java/com/example/aad_team_51_animation_challenge/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.aad_team_51_animation_challenge;
 
-import android.net.Uri;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements WelcomeFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
     }
 }
